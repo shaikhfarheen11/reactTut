8f546c84c6c3d3ec9eb7f7a10eff4530baca0803
shaikhfarheen11 <farheens554>
2023-06-18 00:44:31 +0530

    first commit

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,21 +1,11 @@
- import './ExpenseItem.css';
- 
- function ExpenseItem(){
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-    const locationOfExpenditure = 'Delhi';
-
+function ExpenseItem(){
     return (
-        <div className = "expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className = "expense-item_description">
-                <h2>{expenseTitle}</h2>
-                <div className = "expense-item_price">${expenseAmount}</div>
-              <div className= "expense-item-location">{locationOfExpenditure}</div>
-
-            </div>
-        </div>
-    )
- }
- export default ExpenseItem;
+        <div>
+   <h2>Expense Items</h2>
+    <h3>Food Rs 10</h3>
+    <h3>Petrol Rs 100</h3>
+    <h3>Movies Rs 200</h3>
+    </div>
+    );
+}
+export default ExpenseItem;
