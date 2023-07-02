ccf3bcd039e98383e7e3c14d68fcf9ed6f76d4fe
shaikhfarheen11 <farheens554>
2023-07-02 22:16:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Expenses from './Components/Expenses/Expenses';
+import Expenses from './Components/ExpenseForm';
 
 
 const App = () => {
---
+++ src/Components/ExpenseForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div className="expense-form">
+      <form>
+        <div className="form-control">
+          <label htmlFor="expenseTitle">Expense Title</label>
+          <input type="text" id="expenseTitle" onChange={handleInputChange} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="expenseAmount">Expense Amount</label>
+          <input type="number" id="expenseAmount" onChange={handleInputChange} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="expenseDate">Expense Date</label>
+          <input type="date" id="expenseDate" onChange={handleInputChange} />
+        </div>
+      </form>
+      <button className="submit-button">Submit</button>
+    </div>
+  );
+};
+
+export default ExpenseForm;
