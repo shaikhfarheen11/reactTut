803340bff3e1bad5d2aace07e0577c9e6b7b275f
shaikhfarheen11 <farheens554>
2023-07-03 21:24:57 +0530

    first commit

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,31 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const handleInputChange = (event) => {
-    console.log(event.target.value);
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const handleTitleChange = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const handleAmountChange = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents form submission and page reload
+    // Do something with the entered data, such as sending it to a server
+    console.log(enteredTitle, enteredAmount, enteredDate);
+    // Reset the form after submission
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
     <div className="expense-form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="expenseTitle">Expense Title</label>
-          <input type="text" id="expenseTitle" onChange={handleInputChange} />
+          <input
+            type="text"
+            id="expenseTitle"
+            value={enteredTitle}
+            onChange={handleTitleChange}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="expenseAmount">Expense Amount</label>
-          <input type="number" id="expenseAmount" onChange={handleInputChange} />
+          <input
+            type="number"
+            id="expenseAmount"
+            value={enteredAmount}
+            onChange={handleAmountChange}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="expenseDate">Expense Date</label>
-          <input type="date" id="expenseDate" onChange={handleInputChange} />
+          <input
+            type="date"
+            id="expenseDate"
+            value={enteredDate}
+            onChange={handleDateChange}
+          />
         </div>
+        <button className="submit-button" type="submit">Add Expense</button>
       </form>
-      <button className="submit-button">App Expense</button>
     </div>
   );
 };
 
 export default ExpenseForm;
-
