0fabcdb20973f06143fb7a45599b8b0aac1adbbb
shaikhfarheen11 <farheens554>
2023-07-08 02:19:59 +0530

    first commit

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -23,46 +23,22 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
 
-          {filteredExpenses.map((expense) => (
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found for the selected year.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
             <ExpenseItem
               key={expense.id}
               title={expense.title}
               amount={expense.amount}
               date={expense.date}
-              filteredYear={filteredYear} // Pass the filtered year as a prop
             />
-))};
+          ))
+        )}
 
-          {/* <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-            location={props.items[0].location}
-           
-           />
-            <ExpenseItem
-             title={props.items[1].title}
-             amount={props.items[1].amount}
-             date={props.items[1].date}
-            location={props.items[1].location}
-            
-             />
-              <ExpenseItem
-             title={props.items[2].title}
-             amount={props.items[2].amount}
-             date={props.items[2].date}
-             location={props.items[2].location}
-        
-           
-             />
-              <ExpenseItem
-             title={props.items[3].title}
-             amount={props.items[3].amount}
-             date={props.items[3].date}
-             location={props.items[3].location}
-             
-             
-             />  */}
+        {filteredExpenses.length === 1 && (
+          <p>"Only single Expense here, Please add more..."</p>
+        )}
       </Card>
     </div>
   );
