5fc274ff8a114fa8827f6e520308470376b1d427
shaikhfarheen11 <farheens554>
2023-07-31 17:13:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Fragment } from "react";
-import Header from "./Components/Layouts/Header";
+import { Fragment } from "react";
+import Header from './Components/Layouts/Header';
+import Meals from './Components/Meals/Meals';
 
 function App(){
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
-};
+}
 
 export default App;
