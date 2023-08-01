f74eb2e1b8f47754eebeb1bfd482cb7271f74b42
shaikhfarheen11 <farheens554>
2023-08-01 19:39:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from "react";
 import Header from './Components/Layouts/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from "./Components/Cart/Cart";
+
 
 function App(){
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
